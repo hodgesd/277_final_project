1374c078a9770fe4e6bbbce49bda35b81274ffc9
Derrick Hodges <hodgesd@gmail.com>
2022-12-08 19:32:10 -0600

    highlight leader in each statistic

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Select from "react-select";
 import nba_stats from "/Users/derrickhodges/github/277_final_project/src/data/nba_stats.json";
 
 function App() {
-  // const stats = test_stats;
   const stats = nba_stats;
 
   const [selectedOption, setSelectedOption] = useState(stats[0]);
@@ -23,11 +22,23 @@ function App() {
       isClearable={true}
       options={stats}
       onChange={setter}
-      getOptionLabel={(option) => option.Player}
-      getOptionValue={(option) => option.ID}
+      getOptionLabel={(stats) => stats.Player}
+      getOptionValue={(stats) => stats.ID}
       placeholder="Select a player"
     />
   );
+
+  // determine if Player A has more points than Player B
+  const setPTS_PlayerA = () => {
+    if (Number(selectedOption.PTS) > Number(selectedPlayerB.PTS)) {
+      console.log("Player A has more points than Player B");
+      return true;
+    } else {
+      console.log("Player B has more points than Player A");
+      return false;
+    }
+  };
+
   return (
     <>
       <div
@@ -48,20 +59,50 @@ function App() {
             <h3 className="my-4 text-center text-2xl">
               {selectedOption && selectedOption.Year}
             </h3>
-            <h3 className="my-4 text-center text-2xl">
-              {selectedOption && selectedOption.PTS}
+            <h3
+              className={`my-4 text-center text-2xl ${
+                selectedOption.PTS > selectedPlayerB.PTS
+                  ? "underline decoration-sky-500"
+                  : ""
+              }`}
+            >
+              {selectedOption.PTS}
             </h3>
-            <h3 className="my-4 text-center text-2xl">
-              {selectedOption && selectedOption.TRB}
+            <h3
+              className={`my-4 text-center text-2xl ${
+                selectedOption.TRB > selectedPlayerB.TRB
+                  ? "underline decoration-sky-500"
+                  : ""
+              }`}
+            >
+              {selectedOption.TRB}
             </h3>
-            <h3 className="my-4 text-center text-2xl">
-              {selectedOption && selectedOption.AST}
+            <h3
+              className={`my-4 text-center text-2xl ${
+                selectedOption.AST > selectedPlayerB.AST
+                  ? "underline decoration-sky-500"
+                  : ""
+              }`}
+            >
+              {selectedOption.AST}
             </h3>
-            <h3 className="my-4 text-center text-2xl">
-              {selectedOption && selectedOption.BLK}
+            <h3
+              className={`my-4 text-center text-2xl ${
+                selectedOption.BLK > selectedPlayerB.BLK
+                  ? "underline decoration-sky-500"
+                  : ""
+              }`}
+            >
+              {selectedOption.BLK}
             </h3>
-            <h3 className="my-4 text-center text-2xl">
-              {selectedOption && selectedOption.STL}
+            <h3
+              className={`my-4 text-center text-2xl ${
+                selectedOption.STL > selectedPlayerB.STL
+                  ? "underline decoration-sky-500"
+                  : ""
+              }`}
+            >
+              {selectedOption.STL}
             </h3>
           </div>
           <div className="mt-20 flex flex-col items-center justify-center">
@@ -79,11 +120,51 @@ function App() {
             <h3 className="my-4 text-center text-2xl">
               {selectedPlayerB.Year}
             </h3>
-            <h3 className="my-4 text-center text-2xl">{selectedPlayerB.PTS}</h3>
-            <h3 className="my-4 text-center text-2xl">{selectedPlayerB.TRB}</h3>
-            <h3 className="my-4 text-center text-2xl">{selectedPlayerB.AST}</h3>
-            <h3 className="my-4 text-center text-2xl">{selectedPlayerB.BLK}</h3>
-            <h3 className="my-4 text-center text-2xl">{selectedPlayerB.STL}</h3>
+            <h3
+              className={`my-4 text-center text-2xl ${
+                selectedOption.PTS < selectedPlayerB.PTS
+                  ? "underline decoration-sky-500"
+                  : ""
+              }`}
+            >
+              {selectedPlayerB.PTS}
+            </h3>
+            <h3
+              className={`my-4 text-center text-2xl ${
+                selectedOption.TRB < selectedPlayerB.TRB
+                  ? "underline decoration-sky-500"
+                  : ""
+              }`}
+            >
+              {selectedPlayerB.TRB}
+            </h3>
+            <h3
+              className={`my-4 text-center text-2xl ${
+                selectedOption.AST < selectedPlayerB.AST
+                  ? "underline decoration-sky-500"
+                  : ""
+              }`}
+            >
+              {selectedPlayerB.AST}
+            </h3>
+            <h3
+              className={`my-4 text-center text-2xl ${
+                selectedOption.BLK < selectedPlayerB.BLK
+                  ? "underline decoration-sky-500"
+                  : ""
+              }`}
+            >
+              {selectedPlayerB.BLK}
+            </h3>
+            <h3
+              className={`my-4 text-center text-2xl ${
+                selectedOption.STL < selectedPlayerB.STL
+                  ? "underline decoration-sky-500"
+                  : ""
+              }`}
+            >
+              {selectedPlayerB.STL}
+            </h3>
           </div>
         </div>
       </div>
