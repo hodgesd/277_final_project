a42e88cf25acd6ba6fcca234a4212708e1661bf9
Derrick Hodges <hodgesd@gmail.com>
2022-12-31 11:42:53 -0600

    added python merge of bb-ref index files

--- src/utils/mergeJSON.py
+++ src/utils/mergeJSON.py
@@ -6,19 +6,25 @@ input_dir = '/Users/derrickhodges/github/277_final_project/src/data'
 output_dir = '/Users/derrickhodges/github/277_final_project/src/combinedData'
 
 # Find all the JSON files in the input directory
-json_files = glob.glob(input_dir + '/*.json')
+json_files = glob.glob(f'{input_dir}/*.json')
+
+
+json_list = []
 
 # Open the output file
-with open(output_dir + '/bb-ref_stats.json', 'w') as outfile:
+with open(f'{output_dir}/bb-ref_stats.json', 'w') as outfile:
     # Loop through the JSON files
     for fname in json_files:
         with open(fname, 'r') as infile:
             # Read the data from the file
             data = json.load(infile)
-            # Write the data to the output file
-            json.dump(data, outfile)
-            # Add a separator between the files
-            outfile.write('\n')
+            json_list.append(data)
+    json_string = json.dumps(json_list)
+    outfile.write(json_string)
+
+# # Write the data to the output file
+# json.dump(data, outfile)
+# Add a separator between the files
 
 # Close the output file
 outfile.close()
